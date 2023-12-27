7782ea36da97b1d49289fc6a0ffa1f1603504432
Ijlal Furqaan <ijlalfurqaan5@gmail.com>
2023-12-27 09:43:58 +0530

    1

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  const [color, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4' >Hello</h1>
+    </>
+  )
+}
+
+export default App
