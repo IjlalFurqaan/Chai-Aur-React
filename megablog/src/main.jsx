a7ecf0f63873c8134658ff739dba22ec45c033b2
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-01-16 14:53:57 +0530

    .

--- megablog/src/main.jsx
+++ megablog/src/main.jsx
@@ -1,15 +1,20 @@
-/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router=createBrowserRouter({
+  
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    {/* <RouterProvider router={router}/> */}
+    <App/>
+     <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
 )
